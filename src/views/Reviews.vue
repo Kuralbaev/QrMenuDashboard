cb15eba8c0a2b25948c85bae3b6f4d91a4ce25e2
bot <kuralbaevtemirlan@gmail.com>
2026-02-02 17:42:38 +0800

    asd

--- src/views/Reviews.vue
+++ src/views/Reviews.vue
@@ -127,7 +127,7 @@
             <div class="p-2 space-y-6 overflow-y-auto flex-1">
 
               <!-- Отзыв -->
-              <div v-if="selectedReview.title" class="bg-gray-50 rounded-xl p-4 border border-gray-100">
+              <div v-if="selectedReview.comment" class="bg-gray-50 rounded-xl p-4 border border-gray-100">
                 <div class="flex items-center gap-2 mb-3">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
